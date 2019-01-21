41173f1909446a0e2790c4a7c63e04da15a33668
MacStudent <macstudent@130-iMacInstructor.local>
2019-01-21 12:15:42 -0500

    added example for automatically visiting facebook.com and trying to login

--- src/Main.java
+++ src/Main.java
@@ -27,9 +27,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 // 3. Enter a username:  jenelle@gmail.com
 // 4. Find the password login box
 // 5. Click on the password box
-// 6. Enter a password:  1111
+// 6. Enter a password:  1111  --->  .sendKeys("1111");
 // 7. Find the login button
-// 8. Click on the login button
+// 8. Click on the login button  ->  .click()
 // 9. Make sure that it goes to the next page
 
 //-------------
@@ -46,28 +46,57 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException{
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
 		
 		// Windows
 	    //System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
 		
+		
+		// 1. Configure Selenium to talk to Chrome
 		System.setProperty("webdriver.chrome.driver","/Users/macstudent/Desktop/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		
-		//String baseUrl = "http://demo.guru99.com/test/newtours/";
-		String baseUrl = "http://www.cestarcollege.com/";
+		// 2. Enter the website you want to go to
+		String baseUrl = "https://www.facebook.com/login/";
 		
-		String expectedTitle = "Welcome: Mercury Tours";
-		String actualTitle = "";
-		
-		// launch Chrome and direct it to the Base URL
+		// 3. Open Chrome and go to the base url;
 		driver.get(baseUrl);
+	
+		// 4. Enter a username
+		// ---------------------
+		// 4a. Find the box
+		WebElement usernameBox = driver.findElement(By.id("email"));
+		// 4b. Put the user name in there
+		usernameBox.sendKeys("jenelle@gmail.com");
+
+		// 5. Enter a password
+		// ---------------------
+		// 5a. Find the password box
+		WebElement passwordBox = driver.findElement(By.id("pass"));
+		// 5b. Put the password in there
+		passwordBox.sendKeys("fakepassword123!!!!@");
+		
+		
+		Thread.sleep(1000);
+		
+		// 6. Press the login button
+		// ---------------------
+		// 6a. Find the login button
+		WebElement loginButton = driver.findElement(By.id("loginbutton"));
+		// 6b. Click the button
+		loginButton.click();
+		
+		// 7. Close the browser
+		Thread.sleep(10000);  //pause for 1 second before closing the browser
+		driver.close();
+		
+		
 		
 		// get the actual value of the title
-		actualTitle = driver.getTitle();
-		System.out.println("Title of webpage: " + actualTitle);
+//		actualTitle = driver.getTitle();
+//		System.out.println("Title of webpage: " + actualTitle);
 		
 
 				
@@ -75,14 +104,14 @@ public class Main {
 		 * compare the actual title of the page with the expected one and print
 		 * the result as "Passed" or "Failed"
 		 */
-		if (actualTitle.contentEquals(expectedTitle)){
-		    System.out.println("Test Passed!");
-		} else {
-		    System.out.println("Test Failed");
-		    }
-		   
-		    //close Chrome
-		    driver.close();
+//		if (actualTitle.contentEquals(expectedTitle)){
+//		    System.out.println("Test Passed!");
+//		} else {
+//		    System.out.println("Test Failed");
+//		    }
+//		   
+//		    //close Chrome
+		   // driver.close();
 		   
 	}
 
